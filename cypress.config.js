a45007f4aca69d58191397d7dbe3db08df6af724
VenuBotla <venu2109@gmail.com>
2024-06-26 09:28:06 +0100

    PSD-2814_Cypress_framework

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,59 @@
 const { defineConfig } = require("cypress");
+const cypressSplit = require('cypress-split')
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+// All the plugin information goes into this function
+async function setupNodeEvents(on, config) {  
+  
+  require('cypress-on-fix')(on);
+  cypressSplit(on, config);
+  on("file:preprocessor", cucumber());
+    
+  // Make sure to retun the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  pageLoadTimeout: 30000,
+  defaultCommandTimeout: 10000,
+
+  // https://github.com/adamgruber/mochawesome
+  reporter: 'mochawesome',
+  reporterOptions: {    
+    reportDir: 'cypress/results',
+    reportPageTitle: 'Cypress e2e test report',
+    reportFilename: "[status]_[datetime]-[name]-report",
+    timestamp: "longDate",
+    embeddedScreenshots: true,
+    useInlineDiffs: true,
+    saveAllAttempts: true,
+    json: true,
+    html: true,
+    overwrite: false
+  },
+
+  env: {    
+    URL: "https://staging.product-safety-database.service.gov.uk",
+    APIURL:"https://staging.product-safety-database.service.gov.uk/api/v1",
+    TAGS: "@APITest" 
+  },
+
+  retries: {
+    runMode: 1,
+    openMode: 1
+  },
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    specPattern: 'cypress/integration/examples/*.js'
-  }
+  // baseUrl: "https://postman-echo.com/",
+   //run multiple folders in e2e
+   // specPattern: ['cypress/e2e/uitests/*.feature','cypress/e2e/accessibilityTests/*.feature','cypress/e2e/integrationTests/*.feature'],
+    specPattern: "**/*.feature",
+    retries: 1,
+
+    setupNodeEvents    
+  },
+   
 });
